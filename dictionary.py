b0f4d3d69708b56f229e91378622e60c38464eef
nikitasurya <ns283@snu.edu.in>
2017-11-13 21:06:25 +0530

    code runs only if internet is working

--- dictionary.py
+++ dictionary.py
@@ -1,21 +1,42 @@
 from sys import argv
-from urllib2 import Request, urlopen
+import urllib2 
 import json
-# script, word =argv
+#script, word = argv
 
-class dictionary(object):
-    def __init__(self, word):
-        self.meaning=word
 
-    def get_definition(self):
-        request = Request('http://api.pearson.com/v2/dictionaries/entries?headword=%r'%self.meaning)
-        response = urlopen(request)
-        kittens = response.read()
-        tmp = json.loads(kittens)
-        tmp.keys()
-        print tmp['results'][0]['senses'][0]['definition']
+#print argv, type(argv), 
+
+def internet_on():
+    try:
+        urllib2.urlopen('http://216.58.192.142', timeout=1)
+        print "Internet is ON"
+        return True
+    except urllib2.URLError as err: 
+    	print "Internet is not working"
+        return False
+
+
+
+class Dictionary(object):
+	def __init__(self, word):
+		self.meaning=word
+
+	def get_definition(self):
+		
+			request = Request('http://api.pearson.com/v2/dictionaries/entries?headword=%r'%self.meaning)
+			response = urlopen(request)
+			read_response = response.read()
+			tmp = json.loads(read_response)
+			tmp.keys()
+			print tmp['results'][0]['senses'][0]['definition']
 #https://www.codecademy.com/courses/python-intermediate-en-6zbLp/0/1?curriculum_id=50ecbb9b71204640240001bf
 #http://developer.pearson.com/apis/dictionaries#/
+	
 
-meaning=dictionary("autism")
-meaning.get_definition()
+x=internet_on()
+if x==True:
+	meaning=Dictionary("autism")
+	meaning.get_definition() 
+
+else:
+	print"please turn on your internet"
